06a235f95689fac54459f6106732b4bf04356c57
tamminen.mikkoj@gmail.com <tamminen.mikkoj@gmail.com>
2018-09-13 12:59:38 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,6 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 function addElementToBeginningOfArray() {
   return chocolateBars.push()
 }
+function destruct() {
+  return chocolateBars.push()
+}
