3bd28daea4868b7310ecade30e6f616396e716c8
tamminen.mikkoj@gmail.com <tamminen.mikkoj@gmail.com>
2018-09-13 13:06:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-function addElementToBeginningOfArray(chocolateBars, x) {
-  return new Array (chocolateBars.unshift(x))
+function addElementToBeginningOfArray(chocolateBars) {
+  return new Array (chocolateBars.unshift())
 }
 function destructivelyAddElementToBeginningOfArray() {
   return [...chocolateBars, chocolateBars.unshift()]
